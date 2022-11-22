e7ae6a2b6e152a86be4fd793716e76ba59a92c17
Pradeep782 <74357885+Pradeep782@users.noreply.github.com>
2022-11-22 12:30:14 -0500

    Delete Operatoroverloaing.java

--- src/Operatoroverloaing.java
+++
@@ -1,9 +0,0 @@
-public class Operatoroverloaing {
-
-    public static void main(String[] args) {
-        int a = 2 + 4;
-        String msg = "Hello "+ "Pradeep";
-        System.out.println("You will receive amount of $" + a);
-        System.out.println("Hey "  +msg);
-    }
-}
